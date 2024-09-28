5d7d5070b8def65e4b97bdd172444c1d23c3f1ab
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2024-09-28 23:48:10 +0700

    add: profile head section UI

--- lib/presentation/profile/profile_page.dart
+++ lib/presentation/profile/profile_page.dart
@@ -6,13 +6,82 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
+    return Scaffold(
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(53),
         child: ProfileAppbar(),
       ),
       body: Column(
-        children: [],
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    margin:
+                        const EdgeInsets.only(right: 20, left: 20, bottom: 8),
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: const Text(
+                      'M',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 4,
+                    right: 18,
+                    child: InkWell(
+                      onTap: () {},
+                      child: Container(
+                        height: 25,
+                        width: 25,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade300,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        child: const Icon(
+                          Icons.camera_alt,
+                          size: 18,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Muhammad Fauzi',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text('Masuk dengan Google')
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
