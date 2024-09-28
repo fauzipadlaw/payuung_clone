6627636c4d626083558dbc8752fc6b0e159ce11f
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2024-09-29 02:16:11 +0700

    WIP: personal info forms

---
+++ lib/presentation/personal_info/components/company_form.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompanyForm extends StatelessWidget {
+  const CompanyForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
